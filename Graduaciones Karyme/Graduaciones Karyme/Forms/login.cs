2c6388eb87fc02a29248198e2180fed771c2432b
Alexis Baltazar Lopez Soberanes <18030050@uadeo.mx>
2020-05-05 03:00:38 -0500

    Inicio de login
    Creacion de primer formulario para ingresar al sistema.
    Uso del framework bunifu para diseño.

---
+++ Graduaciones Karyme/Graduaciones Karyme/Forms/login.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graduaciones_Karyme.Forms
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
